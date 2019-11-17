f39c0cc612b77d78c14b941ab2f8c2bfdca916e0
directman66 <xpenn@e1.ru>
2019-11-17 18:26:04 +0500

    commit message

---
+++ modules/phpmyadmin/phpmyadmin.class.php
@@ -0,0 +1,177 @@
+<?php
+/**
+* kodexplorer
+* @package project
+* @author Wizard <sergejey@gmail.com>
+* @copyright http://majordomo.smartliving.ru/ (c)
+* @version 0.1 (wizard, 20:06:31 [Jun 01, 2018])
+*/
+//
+//
+class phpmyadmin extends module {
+/**
+* webterminal
+*
+* Module class constructor
+*
+* @access private
+*/
+function phpmyadmin() {
+  $this->name="phpmyadmin";
+  $this->title="phpmyadmin";
+  $this->module_category="<#LANG_SECTION_SYSTEM#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=1) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+//  global $login;
+//  global $pwd;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+	
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $out['TAB']=$this->tab;	
+  
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ $this->getConfig();
+ $out['login'] = $this->config['login'];
+ $out['pwd'] = $this->config['pwd'];
+  $out['type']=$this->config['type'];			
+$out['typefm']=$this->config['typefm'];				
+
+ if ($this->view_mode=='update_settings') {
+	global $login;
+	$this->config['login']=$login;	 
+
+	global $pwd;
+	$this->config['pwd']=$pwd;	 
+	 
+	global $type;
+	$this->config['type']=$type;	
+	 
+	global $typefm;
+	$this->config['typefm']=$typefm;	 
+
+	 
+	 
+   $this->saveConfig();
+   $this->redirect("?");
+ }
+
+
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ $this->admin($out);
+}
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($data='') {
+  parent::install();
+ }
+
+ function uninstall() {
+ parent::uninstall();
+ }
+
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgSnVuIDAxLCAyMDE4IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
